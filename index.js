09f874352e5362e4c24118fefc783e7452b2a3d0
marioharyzal <marioharyzal11@gmail.com>
2022-02-23 22:03:57 +0700

    upgrade to v2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const text = document.getElementById("text");
+const button = document.getElementById("button");
 
-document.getElementById("button").addEventListener("click", () => {
+button.addEventListener("click", () => {
     if (text.innerHTML == "Selamat datang") {
-        text.innerHTML = "Wellcome"; 
+        text.innerHTML = "Wellcome";
+        button.innerHTML = "Translate to indonesia";
+
     } else if (text.innerHTML == "Wellcome") {
         text.innerHTML = "Selamat datang"; 
+        button.innerHTML = "Translate to english";
     } 
 
 });
